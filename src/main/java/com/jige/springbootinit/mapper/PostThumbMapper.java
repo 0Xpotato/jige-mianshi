cf7da5c02f5ef7efb1e4abe8187d78783348b965
jige1024 <2576366072@qq.com>
2025-04-22 22:54:43 +0800

    --------jige-mianshi -------项目初始化

---
+++ src/main/java/com/jige/springbootinit/mapper/PostThumbMapper.java
@@ -0,0 +1,17 @@
+package com.jige.springbootinit.mapper;
+
+import com.jige.springbootinit.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/jige/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.jige.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jige.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
